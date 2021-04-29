2d9de2b9df5c201ac1ab32b5b442ecff76e456e8
ilyukevich <9009901@gmail.com>
2021-04-29 17:30:32 +0300

    upd utils

--- recipes/utils.py
+++ recipes/utils.py
@@ -2,35 +2,43 @@ from recipes.models import Recipe
 
 
 def tags_filter(request, author=None, favourites=None):
-    """ Filter recipes by tags"""
+    """ Filter recipes by tags 'breakfast', 'lunch', 'dinner'"""
 
-    state = ['breakfast', 'lunch', 'dinner']
-    breakfast = request.GET.get('breakfast')
-    lunch = request.GET.get('lunch')
-    dinner = request.GET.get('dinner')
-    if breakfast:
-        state[0] = None
-    if lunch:
-        state[1] = None
-    if dinner:
-        state[2] = None
+    # state = ['breakfast', 'lunch', 'dinner']
+    # breakfast = request.GET.get('breakfast')
+    # lunch = request.GET.get('lunch')
+    # dinner = request.GET.get('dinner')
+    # if breakfast:
+    #     state[0] = None
+    # if lunch:
+    #     state[1] = None
+    # if dinner:
+    #     state[2] = None
+
+    tags_for_filter = []
+    tag_list = ['breakfast', 'lunch', 'dinner']
+
+    for tag_name in tag_list:
+        add_tag = request.GET.get(tag_name)
+        if add_tag:
+            tags_for_filter.append(add_tag)
 
     if author:
         recipes = (Recipe.objects
                    .select_related('author')
                    .prefetch_related('recipe_ingredient')
-                   .filter(tag__overlap=state, author__username=author))
+                   .filter(tag__overlap=tags_for_filter, author__username=author))
     elif favourites:
         recipes = (Recipe.objects
                    .select_related('author')
                    .prefetch_related('recipe_ingredient')
-                   .filter(tag__overlap=state, favourite__in=favourites))
+                   .filter(tag__overlap=tags_for_filter, favourite__in=favourites))
     else:
         recipes = (Recipe.objects
                    .select_related('author')
                    .prefetch_related('recipe_ingredient')
-                   .filter(tag__overlap=state))
+                   .filter(tag__overlap=tags_for_filter))
 
-    tag = {key: 'tags__checkbox_active' for key in state}
+    tag = {key: 'tags__checkbox_active' for key in tags_for_filter}
 
     return recipes, tag
