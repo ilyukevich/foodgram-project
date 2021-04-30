ccc26aa66156f44507f32f1efb7924ee0de26ab5
ilyukevich <9009901@gmail.com>
2021-04-30 10:50:09 +0300

    upd utils and add local

--- recipes/utils.py
+++ recipes/utils.py
@@ -4,13 +4,13 @@ from recipes.models import Recipe
 def tags_filter(request, author=None, favourites=None):
     """ Filter recipes by tags 'breakfast', 'lunch', 'dinner'"""
 
-    tags_for_filter = []
     tag_list = ['breakfast', 'lunch', 'dinner']
+    tags_for_filter = tag_list.copy()
 
     for tag_name in tag_list:
-        add_tag = request.GET.get(tag_name)
-        if add_tag:
-            tags_for_filter.append(add_tag)
+        rm_tag = request.GET.get(tag_name)
+        if rm_tag:
+            tags_for_filter.remove(rm_tag)
 
     if author:
         recipes = (Recipe.objects
